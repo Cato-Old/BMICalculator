51c751836beb26b97211dc6092477660451a73d8
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2018-12-08 11:29:41 +0100

    adding test for BMICalculator class

--- src/main/java/Calculator/BMICalculator.java
+++ src/main/java/Calculator/BMICalculator.java
@@ -1,1 +1,23 @@
-package Calculator;public class BMICalculator implements Calculator {    private int weight;    private int height;    BMICalculator(int weight, int height){        this.weight = weight;        this.height = height;    }    @Override    public String calculate() {        return null;    }    @Override    public String interpret() {        return null;    }}
+package Calculator;
+
+public class BMICalculator implements Calculator {
+    private int weight;
+    private int height;
+
+    public BMICalculator(int weight, int height){
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public BMICalculator(){}
+
+    @Override
+    public String calculate() {
+        return null;
+    }
+
+    @Override
+    public String interpret() {
+        return null;
+    }
+}
